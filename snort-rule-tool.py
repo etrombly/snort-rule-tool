9a56fe2e51d440a920329c8d7a3b28e2a9121131
etrombly <etrombly@yahoo.com>
2015-02-20 03:13:15 +0100

    added recursive searching and nocase

--- snort-rule-tool.py
+++ snort-rule-tool.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 
 import sys
+import math
 from PyQt5 import QtWidgets
 from PyQt5 import QtGui
 from PyQt5 import QtCore
@@ -25,6 +26,7 @@ class Snort(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.show()
         self.index = 0
+        self.comboBoxes = [self.ui.srcCombo, self.ui.srcPortCombo, self.ui.destCombo, self.ui.destPortCombo]
         self.defaultFmt = self.ui.hexColumn.currentCharFormat()
         self.ui.packetBox.valueChanged.connect(self.changePacket)
         self.ui.actionOpen.triggered.connect(self.openPCAP)
@@ -32,6 +34,7 @@ class Snort(QtWidgets.QMainWindow):
         self.ui.flowCheck.stateChanged.connect(self.flowChecked)
         self.ui.streamButton.clicked.connect(self.assembleStream)
         self.ui.flowCombo.currentTextChanged.connect(self.buildRule)
+        self.ui.actionCombo.currentTextChanged.connect(self.buildRule)
         self.ui.protoCombo.currentTextChanged.connect(self.buildRule)
         self.ui.srcCombo.currentTextChanged.connect(self.buildRule)
         self.ui.srcPortCombo.currentTextChanged.connect(self.buildRule)
@@ -156,25 +159,108 @@ class Snort(QtWidgets.QMainWindow):
             self.file = filename[0]
             self.packets = rdpcap(self.file)
             self.findStreams()
-            self.comboBoxes = [self.ui.srcCombo, self.ui.srcPortCombo, self.ui.destCombo, self.ui.destPortCombo]
             self.ui.packetBox.setRange(1, len(self.packets))
             self.readPacket()
 
     def contentChanged(self):
         content = self.ui.contentEdit.text()
-        hexContent = self.ui.hexColumn.toPlainText()
+        hexContent = self.ui.hexColumn.toPlainText().replace("\n", "")
+        textContent = self.ui.textColumn.toPlainText().replace("\n", "")
+        if self.ui.nocaseCheck.isChecked():
+            content = content.lower()
+            textContent = textContent.lower()
         cursor = QtGui.QTextCursor(self.ui.hexColumn.document())
         cursor.setPosition(0, QtGui.QTextCursor.MoveAnchor)
         cursor.setPosition(self.ui.hexColumn.document().characterCount() - 1, QtGui.QTextCursor.KeepAnchor)
         cursor.setCharFormat(self.defaultFmt)
-        if content in hexContent:
-            start = hexContent.index(content)
-            end = len(content) + start
+        cursor2 = QtGui.QTextCursor(self.ui.textColumn.document())
+        cursor2.setPosition(0, QtGui.QTextCursor.MoveAnchor)
+        cursor2.setPosition(self.ui.textColumn.document().characterCount() - 1, QtGui.QTextCursor.KeepAnchor)
+        cursor2.setCharFormat(self.defaultFmt)
+        matchPointer = 0
+        endPointer = 0
+        start = 0
+        end = 0
+        match = False
+        origContent = content
+        while content:
+            print "searching for %s at %s" % (content, endPointer)
+            if content.startswith("|"):
+                if content.count("|") > 1:
+                    content = content[1:]
+                    index = content.index("|")
+                    search = content[0:index]
+                    content = content[index + 1:]
+                else:
+                    search = content[1:]
+                    content = None
+                if search and \
+                  (not match and search in hexContent[endPointer:]) or \
+                  (match and hexContent[endPointer:endPointer + len(search)] == search):
+                    if not match:
+                        end = hexContent[endPointer:].index(search) + len(search) + endPointer
+                        start = hexContent[endPointer:].index(search) + endPointer
+                        match = True
+                        matchPointer = end
+                    else:
+                        end = end + len(search)
+                    endPointer = end
+                elif match:
+                    print "resetting"
+                    content = origContent
+                    match = False
+                    start = 0
+                    end = 0
+                    endPointer = matchPointer
+                    matchPointer = 0
+                else:
+                    print start, end, matchPointer, endPointer, "'%s'" % (hexContent[endPointer:len(search) + endPointer], ), search
+                    break
+            else:
+                if "|" in content:
+                    search = content[0:content.index("|")]
+                    content = content[content.index("|"):]
+                else:
+                    search = content
+                    content = None
+                textPointer = int(math.ceil(endPointer / 3.0))
+                if search and \
+                  (not match and search in textContent[textPointer:]) or \
+                  (match and textContent[textPointer:len(search) + textPointer] == search):
+                    if not match:
+                        end = ((textContent[textPointer:].index(search) + len(search)) * 3) + endPointer
+                        start = (textContent[textPointer:].index(search) * 3) + endPointer
+                        match = True
+                        matchPointer = end
+                    else:
+                        end = end + (len(search) * 3) + 1
+                    endPointer = end
+                elif match:
+                    print "resetting"
+                    content = origContent
+                    match = False
+                    start = 0
+                    end = 0
+                    endPointer = matchPointer
+                    matchPointer = 0
+                else:
+                    print start, end, matchPointer, endPointer, textContent[textPointer:len(search) + textPointer], search
+                    break
+        if match:
+            start = start + (start / 47)
+            end = end + (end / 47)
+            print "Highlighting %s to %s" % (start, end)
+            print
             fmt = QtGui.QTextCharFormat()
             fmt.setForeground(QtCore.Qt.red)
             cursor.setPosition(start, QtGui.QTextCursor.MoveAnchor)
             cursor.setPosition(end, QtGui.QTextCursor.KeepAnchor)
             cursor.setCharFormat(fmt)
+            cursor2.setPosition(start / 3, QtGui.QTextCursor.MoveAnchor)
+            cursor2.setPosition(math.ceil(end / 3.0), QtGui.QTextCursor.KeepAnchor)
+            cursor2.setCharFormat(fmt)
+            self.ui.depthEdit.setText(str(int(math.ceil(end / 3.0))))
+            self.ui.offsetEdit.setText(str(start / 3))
 
     def clearAll(self):
         for combo in self.comboBoxes:
@@ -189,6 +275,19 @@ class Snort(QtWidgets.QMainWindow):
         self.ui.flowCombo.setCurrentText("established")
         self.ui.flowCombo.setEnabled(False)
         self.ui.streamButton.setEnabled(False)
+        self.ui.depthCheck.setChecked(False)
+        self.ui.depthEdit.clear()
+        self.ui.depthEdit.setEnabled(False)
+        self.ui.offsetCheck.setChecked(False)
+        self.ui.offsetEdit.clear()
+        self.ui.offsetEdit.setEnabled(False)
+        self.ui.distanceCheck.setChecked(False)
+        self.ui.distanceEdit.clear()
+        self.ui.distanceEdit.setEnabled(False)
+        self.ui.withinCheck.setChecked(False)
+        self.ui.withinEdit.clear()
+        self.ui.withinEdit.setEnabled(False)
+        self.ui.nocaseCheck.setChecked(False)
 
     def flowChecked(self):
         self.ui.flowCombo.setEnabled(self.ui.flowCheck.isChecked())
